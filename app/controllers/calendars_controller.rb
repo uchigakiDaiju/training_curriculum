e5463f9917e29a8822022a77a8f536bbc39512ea
uchigakiDaiju <uchigaki.daiju@gmail.com>
2020-11-05 20:36:49 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,9 +35,24 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day,
+               :day_of_the_week => wdays[(@todays_date + x).wday],:plans => today_plans}
       @week_days.push(days)
     end
 
   end
 end
+
+
+# require 'date'
+
+# a = Date.new(1993, 2, 24)
+# b = Date.parse('1993-02-24')
+# b += 10
+
+# b - a            #=> 10
+# b.year           #=> 1993
+# b.strftime('%a') #=> "Sat" 
+#これではダメだったか。
+
+# yesterday = Date.today - 1
